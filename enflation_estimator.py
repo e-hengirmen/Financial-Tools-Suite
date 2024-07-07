b50f4c599200d15f189081c851a3d7c6a15f3af2
e-hengirmen <ehengirmen@hotmail.com>
2024-07-07 16:53:01 +0300

    roi calculater bug fix + new enflation estimator added

--- enflation_estimator.py
+++ enflation_estimator.py
@@ -18,13 +18,22 @@ class EnflationEstimateYearly:
     ]
     def __init__(self, enflation_estimation_list=[]):
         if enflation_estimation_list:
-            self.enflation_estimator_list = enflation_estimation_list
+            self.enflation_estimation_list = enflation_estimation_list
         self.value_loss_rate_list = [1/i**(1/12) for i in self.enflation_estimation_list]
     def __getitem__(self, month):
         index = month//12
         try:
             return self.value_loss_rate_list[index]
         except:
-            raise Exception(f'there are only {len(value_loss_rate_list)*12} months of value. You asked for month {month}')
+            raise Exception(f'there are only {len(self.value_loss_rate_list)*12} months of value. You asked for month {month}')
     def get_yearly_enflation_list(self):
         return self.enflation_estimation_list
+
+class EnflationEstimateFlat:
+    def __init__(self, enflation_estimate=1.1):
+        self.enflation_estimate = enflation_estimate
+        self.value_loss_rate = 1/enflation_estimate**(1/12)
+    def __getitem__(self, month):
+        return self.value_loss_rate
+    def get_yearly_enflation_list(self):
+        return [self.enflation_estimate for i in range(15)]
--- roi_calculator.py
+++ roi_calculator.py
@@ -1,12 +1,6 @@
 from enflation_estimator import EnflationEstimateYearly
 from rent_estimator import RentEstimatorInitialFlat
 
-M=3000000  # initil credit
-n=120      # months
-r=1.0069   # interest rate
-rent = 25000
-sell_after = 5 # as years. Dont make it more than credit time(because of missing enflation data)
-
 
 class ROICalculator:
   
@@ -20,36 +14,14 @@ class ROICalculator:
     def calculate_roi(self):
         total_payment_value = 0
         monthly_payment_value = self.monthly_payment
-        total_payment = self.monthly_payment*n
-        for current_month in range(n):
+        total_payment = self.monthly_payment * self.n
+        for current_month in range(self.n):
             value_loss_rate = self.enflation_estimator[current_month]
             total_payment_value += monthly_payment_value
             monthly_payment_value *= value_loss_rate
         total_rent = 0
         total_rent_value = 0
-        if rent_estimator:
-            total_rent = rent_estimator.get_total_rent()
-            total_rent_value = rent_estimator.get_total_rent_value()
+        if self.rent_estimator:
+            total_rent = self.rent_estimator.get_total_rent()
+            total_rent_value = self.rent_estimator.get_total_rent_value()
         return self.monthly_payment, total_payment, total_payment_value, self.M/total_payment_value, (self.M+total_rent_value)/total_payment_value, total_rent, total_rent_value
-
-enflation_estimator = EnflationEstimateYearly()
-rent_estimator = RentEstimatorInitialFlat(rent, n, sell_after, enflation_estimator)
-roi_calculator = ROICalculator(M,n,r,enflation_estimator, rent_estimator)
-(
-    monthly_payment,
-    total_payment,
-    total_payment_value,
-    ROI,
-    ROI_with_rent,
-    total_rent,
-    total_rent_value,
-) = roi_calculator.calculate_roi()
-print("monthly payment\t\t", monthly_payment)
-print("total_payment\t\t", total_payment)
-print("total_payment_value\t", total_payment_value)
-print()
-print("ROI\t\t\t", ROI, "(assuming home values with enflation)")
-print("ROI_with_rent\t\t", ROI_with_rent, "(assuming home values with enflation and rent increases yearly with enflation)")
-print()
-print("total_rent\t\t", total_rent)
-print(f"total_rent_value\t", total_rent_value)
